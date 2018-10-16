adfe23118dbe6ac2d298e660f70107495b62eb3d
Robert <robert.a.argume@gmail.com>
2018-10-16 01:53:49 -0400

    Update DB connection data for JAWSDB Heroku MySQL

--- src/config/database.php
+++ src/config/database.php
@@ -1,11 +1,19 @@
 <?php
 class Database{
- 
     // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "PullPrototype";
-    private $username = "root";
-    private $password = "123";
+
+    // Using parameters for local MySQL database
+    // private $host = "localhost";
+    // private $db_name = "PullPrototype";
+    // private $username = "root";
+    // private $password = "123";
+    // public $conn;
+    
+    // Using parameters from JAWSDB_URL external database for Heroku
+    private $host = "u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    private $db_name = "sz5wc58ptown98vx";
+    private $username = "k7y4qtya1q9jjbp5";
+    private $password = "po9tod5cb2buvzni";
     public $conn;
  
     // get the database connection
